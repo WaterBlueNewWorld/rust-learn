cc7e9fbfc7ec06dad3b0af08c0e45215b765b2f4
Alejandro Aviña <alejandroj@tuta.io>
2021-05-21 09:22:41 -0600

    XD

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut text = String::new();
-    let _intext = std::io::stdin().read_line(&mut text).unwrap();
     println!("Ingresa una palabra");
+    let _intext = std::io::stdin().read_line(&mut text).unwrap();
     let _state = match text.trim() {
         "XD" => println!("LOOOOLOLOLOLOLXDXDDX"),
         "HGDFJD" => println!("SAS"),
